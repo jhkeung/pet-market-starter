eeb96255612ec7e0f685f9f3ce4a72073d019db6
Jocelyn Keung <jocelyn.keung@captechventures.com>
2017-11-04 12:27:57 -0400

    Adding components and non-ng html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PetDetailComponent } from './pet-detail/pet-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { ApplicationComponent } from './application/application.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    GalleryComponent,
+    PetDetailComponent,
+    HeaderComponent,
+    ApplicationComponent
   ],
   imports: [
     BrowserModule,
